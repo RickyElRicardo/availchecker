7ea5b574d5058efa5dc1a845c3bda3e5387c8edf
Ricardo Thousand <Rickmasta@gmail.com>
2011-08-08 22:21:07 -0400

    random stuff

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -4,8 +4,9 @@ class Domain < ActiveRecord::Base
 
 	validates :email, :email => true
 
-	validates_format_of :url, :with => /^[^\-](?![^\.]+-\.)[a-z0-9\-]+\.[a-z0-9]+/i
-	end
+	validates_format_of :domain, :with => /^[^\-](?![^\.]+-\.)[a-z0-9\-]+\.[a-z0-9]+/i, :message => "Invalid Domain. Domain names only include A-Z, 0-9, and - (dash). Please do not include the 'www.', 'http://' or anything infrom of the domain. Example: 'domain.com', 'website.net', 'url.org' "
+
+
 
 	def check_domains
 		Domain.where(:confirmed => "yes").each do |f|
