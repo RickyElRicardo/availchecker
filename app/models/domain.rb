27e53717f7fbad066dd277763b1cb090857459af
Ricardo Thousand <rickmasta@gmail.com>
2011-08-14 19:11:33 -0400

    fixed check_domains method (in domains.rb)

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -9,17 +9,17 @@ class Domain < ActiveRecord::Base
 
 
 	def check_domains
-      @domains = Domain.all
-
-      #@domains.where(:confirmed => "yes").each do |f|
-      @domains.each do |f|
-        begin
-          r = Whois.whois(f.domain)
-          if r.available? == true
-            EmailNotify.notify_email(f).deliver
-          end
-        rescue 
-          next  #do something here like re raise the error or store the email address in a bad_emails table or do both just simply do nothing at all
+    @domains = Domain.all
+    #@domains.where(:confirmed => "yes").each do |f|
+    @domains.each do |f|
+      begin
+        r = Whois.whois(f.domain)
+        if r.available? == true
+          EmailNotify.notify_email(f).deliver
         end
+      rescue 
+        next  #do something here like re raise the error or store the email address in a bad_emails table or do both just simply do nothing at all
       end
     end
+  end
+end
