b10e9fc360ff1cfb1174f04286b1abbce0ed959c
Ricardo <Rickmasta@users.noreply.github.com>
2017-01-25 19:55:06 -0500

    Update setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
       :address              => "smtp.gmail.com",  
       :port                 => 587,  
       :domain               => "gmail.com",  
-     :user_name            => "availchecker", #Your user name
-      :password             => "l", # Your password
+     :user_name            => "", #Your user name
+      :password             => "", # Your password
       :authentication       => "plain",  
       :enable_starttls_auto => true  
    }
